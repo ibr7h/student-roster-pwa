53fe95d51fdc82534cbe274c44bb04a9c8730dfc
Ibrahim Alneami <d.alneami@gmail.com>
2026-09-20 15:14:02 +0300

    Force service worker refresh for v4.1.1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// release 4.1.0
+// release 4.1.1
 importScripts('./version.js');
 const CACHE='student-roster-pwa-v'+(self.APP_VERSION||'3.10.0');
 const ASSETS=['./','./index.html','./styles.css','./print.css','./app.js','./version.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./assets/moe-logo.png'];
