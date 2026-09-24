e36f2fd8ca73f2b39ca0c3232e6ba7c4ca435b67
Ibrahim Alneami <d.alneami@gmail.com>
2026-09-24 19:53:19 +0300

    v4.18.0 bottom app bar: sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-// release 4.17.1 searchable-behavior-violations
+// release 4.18.0 integrated-shifting-bottom-app-bar
 importScripts('./version.js');
-const CACHE='student-roster-pwa-v'+(self.APP_VERSION||'4.17.1');
+const CACHE='student-roster-pwa-v'+(self.APP_VERSION||'4.18.0');
 const ASSETS=['./','./index.html','./styles.css','./behavior.css','./print.css','./app.js','./behavior.js','./version.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./assets/moe-logo.png'];
 
 async function broadcastUpdate(payload){
